7ac354c7fe4d23f8ff82cd4532f88305fbc4ac5f
Cory Todd <cory@corytodd.us>
2026-03-29 07:40:16 -0700

    chore: rename conduit to tuwunel
    No sense keeping 3 names for the same service. This also fixes the issue
    where on fresh deployment the caddy and tuwunel directories have the
    wrong ownership and therefore incorrect docker mount point.

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -8,8 +8,9 @@ $Remote = "$($env:USER)@$($env:HOST)"
 $Dest = $env:DEST
 $Sha = git rev-parse HEAD
 
+ssh $Remote "mkdir -p $Dest/caddy $Dest/tuwunel"
 scp docker-compose.yml "${Remote}:${Dest}/docker-compose.yml"
 scp caddy/Caddyfile "${Remote}:${Dest}/caddy/Caddyfile"
-scp conduit/conduit.toml "${Remote}:${Dest}/conduit/conduit.toml"
+scp tuwunel/tuwunel.toml "${Remote}:${Dest}/tuwunel/tuwunel.toml"
 
-ssh $Remote "cd $Dest && GIT_SHA=$Sha docker compose up -d --pull always"
+ssh $Remote "cd $Dest && GIT_SHA=$Sha docker compose up -d --pull always --remove-orphans"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,9 @@ set +a
 
 SHA=$(git rev-parse HEAD)
 
+ssh "${USER}@${HOST}" "mkdir -p ${DEST}/caddy ${DEST}/tuwunel"
 scp docker-compose.yml "${USER}@${HOST}:${DEST}/docker-compose.yml"
 scp caddy/Caddyfile "${USER}@${HOST}:${DEST}/caddy/Caddyfile"
-scp conduit/conduit.toml "${USER}@${HOST}:${DEST}/conduit/conduit.toml"
+scp tuwunel/tuwunel.toml "${USER}@${HOST}:${DEST}/tuwunel/tuwunel.toml"
 
-ssh "${USER}@${HOST}" "cd ${DEST} && GIT_SHA=${SHA} docker compose up -d --pull always"
+ssh "${USER}@${HOST}" "cd ${DEST} && GIT_SHA=${SHA} docker compose up -d --pull always --remove-orphans"
